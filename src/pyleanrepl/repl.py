e09ba476ccd1b0c1d12d302ed65cb60b88e20659
Nilay Patel <nilaypatel2@gmail.com>
2024-08-13 16:04:59 -0700

    Attempt to add project path

--- src/pyleanrepl/repl.py
+++ src/pyleanrepl/repl.py
@@ -11,9 +11,11 @@ class LeanRepl:
     proc: sp.Popen[str]
     env_id: int
     repl_path: str | Path
+    project_path: str | Path
 
-    def __init__(self, repl_path: str | Path):
+    def __init__(self, repl_path: str | Path, project_path : str | Path):
         self.repl_path = repl_path
+        self.project_path = project_path
 
     def __enter__(self):
         self.open()
@@ -25,6 +27,7 @@ class LeanRepl:
     def open(self):
         self.proc = sp.Popen(
             ["lake", "env", f"{self.repl_path}/.lake/build/bin/repl"],
+            cwd=self.project_path,
             stdin=sp.PIPE,
             stdout=sp.PIPE,
             stderr=sp.PIPE,
@@ -78,10 +81,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--repl", "-r", type=str, default="repl", help="path to lean repl dir (which has been built with `lake build`)"
     )
+    parser.add_argument(
+        "--project", "-p", type=str, default=".", help="path to lean repl dir (which has been built with `lake build`)"
+    )
     parser.add_argument("lean", type=str, help="input file or Lean code to run through the repl")
     parser.add_argument("--output", "-o", type=FileType("w"), help="output path, default is stdout", default="-")
 
     args = parser.parse_args()
-    with LeanRepl(args.repl) as repl:
+    with LeanRepl(args.repl, args.project) as repl:
         output = repl.interact(args.lean)
         print(json.dumps(output), flush=True, file=args.output)
