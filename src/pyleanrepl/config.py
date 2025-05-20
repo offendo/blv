807fb0b06a342ed8eeca580b705709bf416f2526
Nilay <nilaypatel2@gmail.com>
2025-05-20 11:08:01 -0700

    Fix wrong environment variable in config.py

--- src/pyleanrepl/config.py
+++ src/pyleanrepl/config.py
@@ -4,6 +4,6 @@ import os
 
 class Config:
     repl_path: str = os.path.expanduser(os.environ['REPL_PATH'])
-    project_path: str = os.path.expanduser(os.environ['REPL_PATH'])
+    project_path: str = os.path.expanduser(os.environ['PROJECT_PATH'])
     backport: bool = False
     imports: list[str] = ['import Mathlib', 'import Aesop']
